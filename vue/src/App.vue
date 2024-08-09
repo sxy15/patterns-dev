717cffaea78c29ba4fa115ad4eee23fcb18ea126
YDONG32 <yuanjie.dong@volvocars.com>
2024-08-09 11:47:06 +0800

    feat: renderless

---
+++ vue/src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+  import ToggleComponent from './renderless-components/ToggleComponent.vue'
+</script>
+
+<template>
+ <ToggleComponent v-slot="{ checkbox, toggleCheckbox }">
+  <div class="comp">
+    <label class="switch">
+      <input type="checkbox" :value="checkbox" @click="toggleCheckbox" />
+      <div class="slider rounded" :class="checkbox ? 'active' : ''"></div>
+    </label>
+  </div>
+ </ToggleComponent>
+
+ <ToggleComponent v-slot="{ checkbox, toggleCheckbox }">
+    <div class="comp">
+      <button class="toggle-button" @click="toggleCheckbox">
+        Toggle | <span>{{ checkbox ? "Yes 😀" : "No 😔" }}</span>
+      </button>
+    </div>
+  </ToggleComponent>
+
+  <ToggleComponent v-slot="{ checkbox, toggleCheckbox }">
+    <div class="comp">
+      <button
+        :class="['tab-button', { active: checkbox }]"
+        @click="toggleCheckbox"
+      >
+        On
+      </button>
+      <button
+        :class="['tab-button', { active: !checkbox }]"
+        @click="toggleCheckbox"
+      >
+        Off
+      </button>
+    </div>
+  </ToggleComponent>
+</template>
+
+<style scoped>
+  @import './style.css';
+</style>
